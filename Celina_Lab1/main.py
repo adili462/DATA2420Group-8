f51303af6d7a24cfc1dcef7a47b1302339fe7c42
cduon916 <cduon916@mtroyal.ca>
2026-09-19 12:15:52 -0600

    Update main.py

--- Celina_Lab1/main.py
+++ Celina_Lab1/main.py
@@ -23,18 +23,47 @@ def parse_row(row: str) -> list:
     """
     values = row.strip().split(",")
 
+
+
     try:
         exam_ID = int(values[0])
+
+        if len(values) != 5:
+                raise MissingValueException(f"Exam no. {exam_ID}: A value is missing.")
+        
         Date = values[1]
         Patient_Name = values[2]
         weight = float(values[3])
         height = float (values[4])
+
     except ValueError:
-        raise TextFormatException()
+        raise TextFormatException(f"Exam no. {exam_ID}: Invalid text format.")
 
     if height > 3:
-        raise MeasurementUnitException
-  
+        raise MeasurementUnitException(f"Exam no. {exam_ID}: A value is in the wrong units.")
+    if weight > 90:
+        raise MeasurementUnitException(f"Exam no. {exam_ID}: A value is in the wrong units.")
+
+    if not(" " in Patient_Name):
+        raise TextFormatException(f"Exam no. {exam_ID}: Patient name formatted incorrectly.")
+    if not("/" in Date):
+        raise TextFormatException(f"Exam no. {exam_ID}: Date formatted incorrectly.")
+
+    
+    check_Date = Date.split("/")
+    for x in range(len(check_Date)):
+        check_Date[x] = float(check_Date[x])
+    if len(check_Date) != 3:
+        raise TextFormatException(f"Exam no. {exam_ID}: Date formatted incorrectly.")
+    if 1 > check_Date[0] or check_Date[0] >12:
+        raise TextFormatException(f"Exam no. {exam_ID}: Date formatted incorrectly.")
+    if 1 > check_Date[1] or check_Date[1]>31:
+        raise TextFormatException(f"Exam no. {exam_ID}: Date formatted incorrectly.")
+    if 2000 > check_Date[2] or check_Date[2]>2026:
+        raise TextFormatException(f"Exam no. {exam_ID}: Date formatted incorrectly.")
+
+    
+    
     return [exam_ID, Date, Patient_Name, weight, height]
 
 
