ce808e96fc899db9569cb8067939037f3c32ec9d
jessikamessika <jess.jm.zhang@gmail.com>
2026-09-23 18:59:47 -0600

    added main.py file where we'll combine our work

---
+++ Lab2_combine/main.py
@@ -0,0 +1,18 @@
+
+# Step 1: Read data from files
+
+# Step 2: Calculate term frequencies
+
+# Step 3: Reduce to unique words
+
+# tie Step 1, 2, and 3 together
+
+
+def main():
+    # Step 1: read the files
+    # Step 2: calculate term frequencies
+    # Step 3: reduce to unique words
+    return
+
+if __name__ == '__main__':
+    main()
